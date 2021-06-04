eb321c03b14888ff06a61b79e4cec6d84944e511
ap <pouillart.alexandre@hotmail.fr>
2021-06-04 10:31:14 +0200

    Changed Home in controllers name

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class CompanyHomeController extends AbstractController
+class CompanyController extends AbstractController
 {
     /**
      * @Route("/company/", name="company_home")
--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class StudentHomeController extends AbstractController
+class StudentController extends AbstractController
 {
     /**
      * @Route("/student/", name="student_home")
