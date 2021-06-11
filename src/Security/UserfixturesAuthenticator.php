c6b06ba6cf4541dce941376835072abd8528e65e
Wowlfy <79702962+Wowlfy@users.noreply.github.com>
2021-06-11 10:37:41 +0200

    Update and rename UserFixturesAuthenticator.php to UserfixturesAuthenticator.php

--- src/Security/UserfixturesAuthenticator.php
+++ src/Security/UserfixturesAuthenticator.php
@@ -20,7 +20,7 @@ use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticato
 use Symfony\Component\Security\Guard\PasswordAuthenticatedInterface;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-class UserFixturesAuthenticator extends AbstractFormLoginAuthenticator implements PasswordAuthenticatedInterface
+class UserfixturesAuthenticator extends AbstractFormLoginAuthenticator implements PasswordAuthenticatedInterface
 {
     use TargetPathTrait;
 
