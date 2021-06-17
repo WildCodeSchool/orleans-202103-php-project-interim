cc31c32d07280069a138db9b3109cf84ad2c8c67
endricklosange <endricklosange@gmail.com>
2021-06-17 17:54:22 +0200

    fix faker

--- src/DataFixtures/CompanyFixtures.php
+++ src/DataFixtures/CompanyFixtures.php
@@ -2,20 +2,22 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use App\Entity\Company;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class CompanyFixtures extends Fixture
 {
     public const LOOPNUMBER = 10;
     public function load(ObjectManager $manager)
     {
-        for ($i = 1; $i <= self ::LOOPNUMBER; $i++) {
+        for ($i = 1; $i <= self::LOOPNUMBER; $i++) {
+            $faker = Factory::create('FR,fr');
             $company = new Company();
-            $company->setCompanyName('Nom de l\'entreprise : ' . $i);
+            $company->setCompanyName($faker->company());
             $company->setSocialReason('HYPERDRIVE EURL : ' . $i);
-            $company->setSiret("12859875326478");
+            $company->setSiret($faker->numberBetween(10000000000000, 99999999999999));
 
             $manager->persist($company);
             $this->addReference('company_' . $i, $company);
--- src/DataFixtures/JobFixtures.php
+++ src/DataFixtures/JobFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use DateTime;
+use Faker\Factory;
 use App\Entity\Job;
 use App\DataFixtures\CompanyFixtures;
 use Doctrine\Persistence\ObjectManager;
@@ -11,18 +12,18 @@ use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
 class JobFixtures extends Fixture implements DependentFixtureInterface
 {
-
     public function load(ObjectManager $manager)
     {
         for ($e = 1; $e <= CompanyFixtures::LOOPNUMBER; $e++) {
+            $faker = Factory::create('FR,fr');
             $job = new Job();
-            $job->setPost('Nom de Poste : ' . $e);
-            $job->setRegisteredAt(new DateTime());
-            $job->setStartAt(new DateTime());
-            $job->setEndAt(new DateTime());
+            $job->setPost($faker->jobTitle());
+            $job->setRegisteredAt($faker->dateTime());
+            $job->setStartAt($faker->dateTime());
+            $job->setEndAt($faker->dateTime());
             $job->setHoursAWeek(rand(25, 35));
-            $job->setPostalCode('45');
-            $job->setCity('Ville');
+            $job->setPostalCode($faker->numberBetween(30, 97));
+            $job->setCity($faker->city());
             $job->setDescription('Veniam qui aliqua deserunt do nisi consectetur pariatur consectetur tempor eiusmod.');
             $job->setCompany($this->getReference('company_' . rand(1, CompanyFixtures::LOOPNUMBER)));
 
