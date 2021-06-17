2495eb3520d6472c2b88042ec7c17f0d9672d91f
endricklosange <endricklosange@gmail.com>
2021-06-17 18:31:30 +0200

    fix_add_faker

--- src/DataFixtures/JobFixtures.php
+++ src/DataFixtures/JobFixtures.php
@@ -15,16 +15,16 @@ class JobFixtures extends Fixture implements DependentFixtureInterface
     public function load(ObjectManager $manager)
     {
         for ($e = 1; $e <= CompanyFixtures::LOOPNUMBER; $e++) {
-            $faker = Factory::create('FR,fr');
+            $faker = Factory::create('fr_FR');
             $job = new Job();
             $job->setPost($faker->jobTitle());
-            $job->setRegisteredAt($faker->dateTime());
-            $job->setStartAt($faker->dateTime());
-            $job->setEndAt($faker->dateTime());
+            $job->setRegisteredAt($faker->dateTimeBetween('2021-01-01 00:00:00', 'now'));
+            $job->setStartAt($faker->dateTimeBetween('2021-01-01 00:00:00', 'now'));
+            $job->setEndAt($faker->dateTimeBetween('2021-01-01 00:00:00', 'now'));
             $job->setHoursAWeek(rand(25, 35));
             $job->setPostalCode($faker->numberBetween(30, 97));
             $job->setCity($faker->city());
-            $job->setDescription('Veniam qui aliqua deserunt do nisi consectetur pariatur consectetur tempor eiusmod.');
+            $job->setDescription($faker->paragraph());
             $job->setCompany($this->getReference('company_' . rand(1, CompanyFixtures::LOOPNUMBER)));
 
             $manager->persist($job);
