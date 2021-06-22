18f5a0595b3a3b0ec64f5bf441329ff509931824
ap <pouillart.alexandre@hotmail.fr>
2021-06-22 11:04:06 +0200

    removed a useless use

--- src/Repository/JobRepository.php
+++ src/Repository/JobRepository.php
@@ -5,7 +5,6 @@ namespace App\Repository;
 use App\Entity\Job;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use App\Entity\CompanyRepository;
 
 /**
  * @method Job|null find($id, $lockMode = null, $lockVersion = null)
