5eec9f825a3e7271e7674101619fbb0c8cced365
ap <pouillart.alexandre@hotmail.fr>
2021-07-06 09:46:55 +0200

    send a mail to postulate to an offer

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Job;
+use App\Entity\User;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StudentController extends AbstractController
 {
@@ -14,5 +19,36 @@ class StudentController extends AbstractController
     public function index(): Response
     {
         return $this->render('student/index.html.twig');
+    }
+
+    /**
+     * @Route("/postulate/{job}", name="postulate")
+     */
+    public function postulate(Request $request, MailerInterface $mailer, Job $job): Response
+    {
+        /** @var User */
+        $user = $this->getUser();
+        // Ici nous enverrons l'e-mail
+        $message = (new Email())
+        //on attribue l'expéditeur
+        ->from(strval($user->getEmail()))
+
+        //on attribue le receveur
+        ->to(strval($this->getParameter('mailer_to')))
+        ->subject('Un étudiant a confirmé vouloir postuler à une annonce')
+
+        //On créé le message avec la vue twig
+        ->html($this->renderView('email/postulateEmail.html.twig', [
+            'user' => $user,
+            'job' => $job,
+        ]));
+
+        //on envoie le message
+        $mailer->send($message);
+
+        // give mesage if success
+        $this->addFlash('success', 'Nous prenons en compte votre demande, 
+        nous vous répondrons dans les meilleurs délais.');
+        return $this->redirectToRoute('offer');
     }
 }
