9257da426ef74134182315fd37b6d30b4d776427
Maxime Guichard <maxime.guichard45@gmail.com>
2021-07-09 15:40:08 +0200

    edit company profile

--- src/Form/UserEditType.php
+++ src/Form/UserEditType.php
@@ -10,7 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
-class StudentEditType extends AbstractType
+class UserEditType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
