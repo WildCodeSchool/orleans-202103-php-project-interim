8abc28f5949d91f1b975ddfa5223e7533170802f
endricklosange <endricklosange@gmail.com>
2021-07-15 12:02:33 +0200

    correction

--- src/Controller/LegalNoticeController.php
+++ src/Controller/LegalNoticeController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/mentions/legales",name="legal_notice_")
+ * @Route("/mentions-legales",name="legal_notice_")
  */
 class LegalNoticeController extends AbstractController
 {
